bca5fa2c880521a782e2a1899b17c3d42d2b2091
Jason Feng <solidzoro@live.com>
2021-12-23 22:16:56 +0800

    chore: clear deps

--- src/routes/error.route.tsx
+++ src/routes/error.route.tsx
@@ -8,12 +8,12 @@ import { Error404 } from '@/page-components/error/Error404/Error404';
 import { routeKey } from './_fn';
 
 const errorRouteList: IRouteItem[] = [
-  // {
-  //   name: 'Error404',
-  //   path: '/*',
-  //   LazyComp: Error404,
-  //   exact: true,
-  // },
+  {
+    name: 'Error404',
+    path: '/*',
+    LazyComp: Error404,
+    exact: true,
+  },
 ];
 
 export const errorRoute = errorRouteList.map((route) => (
--- src/routes/test.route.tsx
+++ src/routes/test.route.tsx
@@ -12,11 +12,11 @@ const testRouteList: IRouteItem[] = [
     path: '/test',
     LazyComp: lazy(() => import(/* webpackChunkName: 'Test' */ '@/page-components/test/Test/Test' ).then((m) => ({ default: m.Test }))), // prettier-ignore
   },
-  // {
-  //   name: 'TestLazyLoadingSpin',
-  //   path: '/test/lazy-loading-spin',
-  //   LazyComp: lazy(() => import(/* webpackChunkName: 'TestLazyLoadingSpin' */ '@/page-components/test/TestLazyLoadingSpin/TestLazyLoadingSpin' ).then((m) => ({ default: m.TestLazyLoadingSpin }))), // prettier-ignore
-  // },
+  {
+    name: 'TestLazyLoadingSpin',
+    path: '/test/lazy-loading-spin',
+    LazyComp: lazy(() => import(/* webpackChunkName: 'TestLazyLoadingSpin' */ '@/page-components/test/TestLazyLoadingSpin/TestLazyLoadingSpin' ).then((m) => ({ default: m.TestLazyLoadingSpin }))), // prettier-ignore
+  },
 ];
 
 export const testRoute = testRouteList.map((route) => (
